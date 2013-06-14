eafc0ec77ef8a3b79d5b603a73b479a433306450
Masahiko Toyoshi <m.yoshi.1700@gmail.com>
2013-06-14 18:13:54 +0900

    experiment zyunbi kanryo!  Please enter the commit message for your changes. Lines starting

--- tools/oneVsAllExam.sh
+++ tools/oneVsAllExam.sh
@@ -3,13 +3,60 @@
 classfilename=$1
 folderfilename=$2
 
-cat ${classfilename}　| while read line
+cat ${classfilename}| while read line
 do
-	classname+=${line}
+	classname+=(${line})
+done
+
+cat ${folderfilename}| while read line
+do
+    foldername+=(`echo "${line}"`)
 done
 
 for i in `seq 1 ${#classname[@]}`
 do
-	echo ${classname[$i-1]}
+    #実験フォルダの準備
+    ./tools/setupExperiment.sh ${classname[$i]}VsAll
+    echo ${classname[$i]}
+    num=`expr ${#foldername[@]} / 2`
+
+    posfolder=0
+    negfolder=0
+
+    for j in `seq 1 $num`
+    do
+
+	k=`expr $j\*2`
+	if [ "${classname[$i]}" = "${foldername[$k-1]}" ]; then
+	    echo ${foldername[$k-1]}_${foldername[$k]}
+	    posfolder=`expr $posfolder + 1`
+	fi
+	if [ "${classname[$i]}" != "${foldername[$k-1]}" ]; then
+	    echo ${foldername[$k-1]}_${foldername[$k]}
+	    negfolder=`expr $negfolder + 1`
+	fi
+    done
+    
+    echo "$posfolder"　> trainData.txt
+    echo "$negfolder" > negDataFolderList.txt
+    for j in `seq 1 $num`
+    do
+	
+	k=`expr $j\*2`
+	if [ "${classname[$i]}" = "${foldername[$k-1]}" ]; then
+	    echo ${foldername[$k-1]}_${foldername[$k]} >> trainData.txt
+	    
+	fi
+	if [ "${classname[$i]}" != "${foldername[$k-1]}" ]; then
+	    echo ${foldername[$k-1]}_${foldername[$k]} >> negDataFolderList.txt
+    
+	fi
+    done
+    mv ./tainData.txt ../${classname[$i]}VsAll/dataset
+    mv ./negDataFolderList.txt ../${classname[$i]}VsAll/negdata
+
+    ../${classname[$i]VsAll}/learning
+    ../${classname[$i]VsAll}/objectPoseEstimation
 done
+
 
