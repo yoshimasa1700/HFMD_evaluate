b44669f3027d7b67783735cb737332af4434ce75
Masahiko Toyoshi <m.yoshi.1700@gmail.com>
2013-06-18 16:38:55 +0900

    add result analyzer

--- tools/analyzeResult.py
+++ tools/analyzeResult.py
@@ -1,25 +1,50 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Jun 16 20:23:11 2013
 
-@author: masahiko
-"""
+import matplotlib.pyplot as plt
 
-dataFile = open('./detectionResult.txt')
+def drange(start, stop, step):
+	r = start
+	while r < stop:
+		yield r
+		r += step
 
-TP = 0
-FP = 0
-FN = 0
-TN = 0
+TPPERT = []
+FPPERT = []
 
-test=[]
+for th in drange(0.00, 0.10 , 0.001):
+	print th
+	resultFile = open('../detectionResult.txt')
+	dataonly = resultFile.readline()
+	data = []
+	TP = 0
+	TN = 0
+	FP = 0
+	FN = 0
 
-for line in dataFile:
-    data = line.split(' ')
-    if data[0] == data[1]:
-        print str('correct')
-        test.append(data)
-    else:
-        print str('incorrect')
+	for line in resultFile:
+		data = line.split()
+		if data[0] == data[1]:
+			if float(data[2]) > th:
+				TP += 1
+			else:
+				FP += 1
+		else:
+			if float(data[2]) > th:
+				TN += 1
+			else:
+				FN += 1
+	resultFile.close()
+	print "TP = %d , FP = %d" % (TP, FP)
+	print "TN = %d , FN = %d" % (TN, FN)
+	precision = float(TP) / ( float(TP) + float(FP) )
+	recall = float(TP) / (float(TP) + float(FN))
 
-dataFile.close()
+	FPF = float(TN) / (float(TN) + float(FN))
+	F = 2 * recall * precision / (recall + precision + 0.000001)
+	accuracy = (float(TP) + float(TN)) / (float(TP) + float(TN) + float(FP) + float(FN))
+	print "precision = %lf , recall = %lf , F = %lf , accuracy = %lf , FPF = %lf" % (precision,recall, F, accuracy, FPF)
+	TPPERT.append(precision)
+	FPPERT.append(FPF)
+
+plt.plot(TPPERT,FPPERT,'r*')
+plt.show()
