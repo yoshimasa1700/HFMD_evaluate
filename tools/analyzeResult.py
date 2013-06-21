a505b904ea994cd19eade12de2c8f77937ff9f90
Masahiko Toyoshi <m.yoshi.1700@gmail.com>
2013-06-21 15:33:49 +0900

    tekitou

--- tools/analyzeResult.py
+++ tools/analyzeResult.py
@@ -27,6 +27,7 @@ ste = 0.1
 preval = 0.0
 bestAccu = 0.0
 bestTh = 0.0
+auc = 0.0
 
 def drange(start, stop, step):
 	r = start
@@ -65,28 +66,28 @@ for th in drange(sta, sto , ste):
 	resultFile.close()
 	print "TP = %d , FP = %d" % (TP, FP)
 	print "TN = %d , FN = %d" % (TN, FN)
-	precision = float(TP) / ( float(TP) + float(FN) + 0.0000001)
-	realprecision = float(TP) / ( float(TP) + float(FN) + 0.000001)
+	precision = float(TP) / ( float(TP) + float(FP) + 0.0000001)
+	sensitivity = float(TP) / ( float(TP) + float(FN) + 0.000001)
 	recall = float(TP) / (float(TP) + float(FN) + 0.0000001)
 
-	FPF = float(TN) / (float(TN) + float(FN) + 0.000000001)
+	specificity = float(TN) / (float(TN) + float(FP) + 0.000000001)
 	F = 2 * recall * precision / (recall + precision + 0.000001)
 	accuracy = (float(TP) + float(TN)) / (float(TP) + float(TN) + float(FP) + float(FN))
         
 	if accuracy > bestAccu:
         	bestAccu = accuracy
                 bestTh = th
-	print "precision = %lf , recall = %lf , F = %lf , accuracy = %lf , FPF = %lf" % (realprecision,recall, F, accuracy, FPF)
+	print "precision = %lf , recall = %lf , F = %lf , accuracy = %lf , specificity = %lf" % (precision,recall, F, accuracy, specificity)
 
-	outFile.write("%lf %lf %lf %lf% lf %lf\n" %(th, precision, recall, F, accuracy, FPF))
-	TPPERT.append(realprecision)
-	FPPERT.append(FPF)
-	FPPERT_SUM += precision * (FPF - preval)
-	preval = FPF
+	outFile.write("%lf %lf %lf %lf% lf %lf\n" %(th, precision, recall, F, accuracy, specificity))
+	TPPERT.append(sensitivity)
+	FPPERT.append(specificity)
+	auc += sensitivity * (specificity - preval)
+	preval = specificity
 
-print FPPERT_SUM
+print auc
 print 'best accuracy = %lf, best th = %lf' % (bestAccu, bestTh)
-outFile.write("%lf"%(FPPERT_SUM))
+outFile.write("%lf"%(auc))
 outFile.write('best accuracy = %lf, best th = %lf' % (bestAccu, bestTh))
 outFile.close()
 
